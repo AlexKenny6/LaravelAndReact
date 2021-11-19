12192d64cd470c1cd2a836668bbffb158b7e9753
AlexKenny6 <alexkenny27@gmail.com>
2021-11-18 23:09:05 -0800

    WIP

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
+});
+
+
+Route::get('posts/{post}', function ($slug) {
+
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if (false === file_exists($path)) {
+
+        return redirect('/');
+        //abort(404);
+
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
 });
